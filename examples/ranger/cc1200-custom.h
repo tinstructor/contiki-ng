5c9356c9b1756f963aab822063e0cf8b4e00b25f
relsas <robbe.elsas@ugent.be>
2019-04-19 15:36:07 +0200

    Added conditional preprocessor for radio driver

---
+++ examples/ranger/cc1200-custom.h
@@ -0,0 +1,8 @@
+#ifndef CC1200_CUSTOM_H_
+#define CC1200_CUSTOM_H_
+
+//extern const cc1200_rf_cfg_t cc1200_802154g_863_870_fsk_50kbps;
+//extern const cc1200_rf_cfg_t cc1200_868_fsk_1_2kbps;
+//extern const cc1200_rf_cfg_t cc1200_868_4gfsk_1000kbps;
+
+#endif /* CC1200_CUSTOM_H_ */
--- examples/ranger/project-conf.h
+++ examples/ranger/project-conf.h
@@ -12,24 +12,32 @@
 
 #define BUTTON_HAL_CONF_WITH_DESCRIPTION    1
 
+#define CC1200_FIXED 1
+
 #undef NETSTACK_CONF_RADIO
 #ifndef RADIO_2400MHZ
-    /*
-    * You will need to configure the defines below to match the configuration of
-    * your sub-ghz network.
-    */
-    #define NETSTACK_CONF_RADIO             cc1200_driver
-    #define CC1200_CONF_RF_CFG              cc1200_802154g_863_870_fsk_50kbps
-    //#define CC1200_CONF_RF_CFG              cc1200_868_fsk_1_2kbps
-    #define ANTENNA_SW_SELECT_DEF_CONF      ANTENNA_SW_SELECT_SUBGHZ
-    #define CC1200_CONF_USE_GPIO2           0
-    #define CC1200_CONF_USE_RX_WATCHDOG     0
-    #define CC1200_CONF_802154G             0
-    #define CC1200_CONF_802154G_CRC16       0
-    #define CC1200_CONF_802154G_WHITENING   0
+    #if CC1200_FIXED
+        #define NETSTACK_CONF_RADIO             cc1200_driver
+        #define CC1200_CONF_RF_CFG              cc1200_802154g_863_870_fsk_50kbps
+        #define ANTENNA_SW_SELECT_DEF_CONF      ANTENNA_SW_SELECT_SUBGHZ
+        #define CC1200_CONF_USE_GPIO2           0
+        #define CC1200_CONF_USE_RX_WATCHDOG     0
+        #define CC1200_CONF_802154G             0
+        #define CC1200_CONF_802154G_CRC16       0
+        #define CC1200_CONF_802154G_WHITENING   0
+    #else
+        #define NETSTACK_CONF_RADIO             cc1200_custom_driver
+        #define CC1200_CONF_RF_CFG              cc1200_802154g_863_870_fsk_50kbps
+        #define ANTENNA_SW_SELECT_DEF_CONF      ANTENNA_SW_SELECT_SUBGHZ
+        #define CC1200_CONF_USE_GPIO2           0
+        #define CC1200_CONF_USE_RX_WATCHDOG     0
+        #define CC1200_CONF_802154G             0
+        #define CC1200_CONF_802154G_CRC16       0
+        #define CC1200_CONF_802154G_WHITENING   0
+    #endif /* #if CC1200_FIXED */
 #else
     #define NETSTACK_CONF_RADIO              cc2538_rf_driver
     #define ANTENNA_SW_SELECT_DEF_CONF       ANTENNA_SW_SELECT_2_4GHZ
-#endif
+#endif /* #if RADIO_2400MHZ */
 
 #endif
