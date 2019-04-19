62a54a15caef79045984e5deb1f66a5e075e58fa
relsas <robbe.elsas@ugent.be>
2019-04-19 11:39:52 +0200

    Preparation for custom ranger project

--- examples/ranger/ranger.c
+++ examples/ranger/ranger.c
@@ -1,53 +1,68 @@
+
+#include <stdio.h>
 #include <assert.h>
 #include <inttypes.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "contiki.h"
+#include "sys/process.h"
+#include "sys/etimer.h"
+#include "sys/log.h"
+#include "dev/gpio-hal.h"
 #include "dev/button-hal.h"
 #include "dev/leds.h"
 #include "dev/radio.h"
 #include "net/netstack.h"
 #include "net/packetbuf.h"
-#include "sys/etimer.h"
+#include "ranger-net.h"
 
-#include "net/nullnet/nullnet.h"
+#ifndef LOG_CONF_LEVEL_RANGER
+#define LOG_CONF_LEVEL_RANGER LOG_LEVEL_NONE
+#endif
 
-#include "sys/log.h"
-#define LOG_MODULE "Ranger"
-#define LOG_LEVEL LOG_LEVEL_INFO
-
-/*----------------------------------------------------------------------------*/
-
-#define UNIQUE_ID UINT32_C(0x931de41a)
-
-#define RX_RECEIVE_LEDS LEDS_GREEN
-#define TX_SEND_LEDS LEDS_RED
-
-#define SEND_GPIO_PORT GPIO_A_NUM
-#define SEND_GPIO_PIN 7
-#define SEND_GPIO_PORT_BASE GPIO_PORT_TO_BASE(SEND_GPIO_PORT)
-#define SEND_GPIO_PIN_MASK GPIO_PIN_MASK(SEND_GPIO_PIN)
+#define LOG_MODULE "RANGER"
+#define LOG_LEVEL LOG_CONF_LEVEL_RANGER
 
 /*----------------------------------------------------------------------------*/
+// Change these for your own use case
+// Make sure that the interval is long enough to send and receive your messages
 
+#define MAIN_INTERVAL_SECONDS   1
+#define MAIN_INTERVAL           (CLOCK_SECOND * MAIN_INTERVAL_SECONDS)
+#define CONTENT_SIZE            32
+#define TX_POWER_DBM            14
+#define CHANNEL                 26
+
+#define ENABLE_SEND_PIN         0
+#define UNIQUE_ID               UINT32_C(0x931de41a)
+#define RX_RECEIVE_LEDS         LEDS_GREEN
+#define TX_SEND_LEDS            LEDS_RED
+
+/*----------------------------------------------------------------------------*/
 PROCESS(ranger_process, "Ranger process");
 AUTOSTART_PROCESSES(&ranger_process);
 
 /*----------------------------------------------------------------------------*/
+extern gpio_hal_pin_t send_pin;
+static process_event_t send_pin_event;
+static gpio_hal_event_handler_t send_pin_event_handler;
 
-enum mode
+static enum
+{
+    MCS_INDEX_0,
+    MCS_INDEX_1,
+    //MCS_INDEX_2,
+    //MCS_INDEX_3,
+    MCS_INDEX_AMOUNT,
+} current_mcs_index;
+
+static enum 
 {
     RX,
     TX,
-};
-
-enum generated_message_type
-{
-    USE_PRESET_MESSAGE,
-    USE_RANDOM_MESSAGE,
-    USE_RANDOM_ASCII_MESSAGE
-};
+    MODE_AMOUNT,
+} current_mode;
 
 typedef struct
 {
@@ -56,12 +71,12 @@ typedef struct
     uint32_t unique_id;
 } message;
 
-static enum mode current_mode;
 static struct etimer message_send_tmr;
+
 static int message_counter = 0;
 
-// Remote ZOL-RM02-B0-240000169 with battery pint has lladress 0012.4b00.09df.4c73
-static const linkaddr_t sender_linkaddr = {{0x00, 0x12, 0x4b, 0x00, 0x09, 0xdf, 0x4c, 0x73}};
+// node labelled "gateway" has link-addr: 0012.4b00.09df.4dee
+// static const linkaddr_t src_linkaddr = {{0x00, 0x12, 0x4b, 0x00, 0x09, 0xdf, 0x4d, 0xee}};
 
 /*----------------------------------------------------------------------------*/
 
@@ -100,16 +115,19 @@ static void fill_message_content(char* buffer, int size)
     }
 }
 
-static void send_message(void)
+static void send_message(const linkaddr_t* dest_addr)
 {
     leds_on(TX_SEND_LEDS);
 
     static uint32_t package_nr_to_send = 0;
 
-    LOG_INFO("Sending message...\n");
+    LOG_INFO("Sending message to ");
+    LOG_INFO_LLADDR(dest_addr);
+    LOG_INFO("\n");
 
     message new_message;
-    fill_message_content(new_message.content, CONTENT_SIZE);
+    memset(new_message.content, 0, CONTENT_SIZE);
+    strncpy(new_message.content, "hello world!", CONTENT_SIZE);
     new_message.package_nr = package_nr_to_send;
     new_message.unique_id = UNIQUE_ID;
     package_nr_to_send++;
@@ -121,10 +139,10 @@ static void send_message(void)
     print_buffer(new_message.content, CONTENT_SIZE, "%2c ");
     LOG_INFO("\\-- Package number: %" PRIu32 "\n", new_message.package_nr);
 
-    nullnet_buf = (uint8_t*) &new_message;
-    nullnet_len = sizeof(new_message);
+    ranger_net_buf = (uint8_t*) &new_message;
+    ranger_net_len = sizeof(new_message);
 
-    NETSTACK_NETWORK.output(NULL);
+    NETSTACK_NETWORK.output(dest_addr);
 
     LOG_INFO("Message sent\n");
 
@@ -136,7 +154,7 @@ static void received_message(const message* current_message, uint16_t datalen)
     if (current_message->unique_id != UNIQUE_ID)
     {
         LOG_WARN("Received message with wrong unique id: got %" PRIx32 ", but expected %" PRIx32 ". Message ignored.\n",
-                 current_message->unique_id,
+                 current_message->unique_id, 
                  UNIQUE_ID);
         return;
     }
@@ -165,45 +183,24 @@ static void received_message(const message* current_message, uint16_t datalen)
     leds_off(LEDS_ALL);
 }
 
-static void received_nullnet_message_callback(const void* data,
+static void received_ranger_net_message_callback(const void* data,
                                               uint16_t datalen,
                                               const linkaddr_t* src,
                                               const linkaddr_t* dest)
 {
-    LOG_INFO("Received message by NULLNET from ");
+    LOG_INFO("Received message from ");
     LOG_INFO_LLADDR(src);
     LOG_INFO_(" to ");
     LOG_INFO_LLADDR(dest);
     LOG_INFO_(" with payload length %" PRIu16 "\n", datalen);
-
-    if (!linkaddr_cmp(src, &sender_linkaddr))
-    {
-        LOG_WARN("Received message from wrong sender: from ");
-        LOG_INFO_LLADDR(src);
-        LOG_WARN_(" but expected ");
-        LOG_INFO_LLADDR(&sender_linkaddr);
-        LOG_WARN_(". Message ignored.\n");
-        return;
-    }
 
     received_message((message*) data, datalen);
 }
 
 static void toggle_mode(void)
 {
-    leds_on(LEDS_ALL);
-
-    if (current_mode == TX)
-    {
-        current_mode = RX;
-        LOG_INFO("Switched to RX Mode\n");
-    }
-    else
-    {
-        current_mode = TX;
-        LOG_INFO("Switched to TX Mode\n");
-    }
-
+    current_mode = (current_mode + 1) % MODE_AMOUNT;
+    LOG_INFO("Switched to %s Mode\n", current_mode == RX ? "RX" : "TX");
     leds_off(LEDS_ALL);
 }
 
@@ -285,60 +282,68 @@ static void set_channel(int channel)
     }
 }
 
-/* static void send_pin_callback(uint8_t port, uint8_t pin) */
-/* { */
-/*     LOG_INFO("Interrupt callback on port %" PRIu8 " pin %" PRIu8 "\n", port, pin); */
-/*     send_message(); */
-/* } */
+static void send_handler(gpio_hal_pin_mask_t pin_mask)
+{
+    process_post(&ranger_process, send_pin_event, NULL);
+}
 
 static void init_send_pin(void)
-{
-    /* // At reset all GPIO pads are set as inputs and are pulled up */
-    /* GPIO_SOFTWARE_CONTROL(SEND_GPIO_PORT_BASE, SEND_GPIO_PIN_MASK); */
-    /* GPIO_SET_INPUT(SEND_GPIO_PORT_BASE, SEND_GPIO_PIN_MASK); */
-    /* GPIO_DETECT_EDGE(SEND_GPIO_PORT_BASE, SEND_GPIO_PIN_MASK); */
-    /* GPIO_TRIGGER_SINGLE_EDGE(SEND_GPIO_PORT_BASE, SEND_GPIO_PIN_MASK); */
-    /* GPIO_DETECT_RISING(SEND_GPIO_PORT_BASE, SEND_GPIO_PIN_MASK); */
-    /*  */
-    /* gpio_register_callback(send_pin_callback, SEND_GPIO_PORT, SEND_GPIO_PIN); */
-    /* // typedef void (* gpio_callback_t)(uint8_t port, uint8_t pin); */
-    /* // TODO(martijn) Geeft warning, maar de signaturen komen wel overeen? */
-    /*  */
-    /* GPIO_ENABLE_INTERRUPT(SEND_GPIO_PORT_BASE, SEND_GPIO_PIN_MASK); */
+{ 
+    gpio_hal_pin_cfg_t send_pin_cfg = GPIO_HAL_PIN_CFG_EDGE_RISING | GPIO_HAL_PIN_CFG_INT_ENABLE |
+        GPIO_HAL_PIN_CFG_PULL_UP;
+
+    send_pin_event = process_alloc_event();
+
+    send_pin_event_handler.pin_mask = 0;
+    send_pin_event_handler.handler = send_handler;
+
+    gpio_hal_arch_pin_set_input(send_pin);
+    gpio_hal_arch_pin_cfg_set(send_pin, send_pin_cfg);
+    gpio_hal_arch_interrupt_enable(send_pin);
+    send_pin_event_handler.pin_mask |= gpio_hal_pin_to_mask(send_pin);
+    gpio_hal_register_handler(&send_pin_event_handler);
 }
 
 static void print_diagnostics(void)
 {
-    printf("\n");
-    printf("Device: %s\n", BOARD_STRING);
-    printf("Message size: %d byte(s)\n", sizeof(message));
-    printf("Message content size: %d byte(s)\n", CONTENT_SIZE);
-    printf("Transmission power: %d dBm\n", TX_POWER_DBM);
-    printf("Channel: %d\n", CHANNEL);
-    printf("Timer period: %d.%02d s\n", ((int) MAIN_INTERVAL_SECONDS), (int) (MAIN_INTERVAL_SECONDS * 100) % 100);
-    printf("\n");
+    LOG_INFO("Device: %s\n", BOARD_STRING);
+    LOG_INFO("Payload size: %zu byte(s)\n", sizeof(message));
+    LOG_INFO("Transmission power: %d dBm\n", TX_POWER_DBM);
+    LOG_INFO("Channel: %d\n", CHANNEL);
+    //LOG_INFO("MCS index: %"PRIu16"\n", get_mcs_index());
+    //LOG_INFO("Bitrate: %lu\n", get_bitrate());
+    //LOG_INFO("Base frequency: %lu\n", get_base_frequency());
+    LOG_INFO("Timer period: %d s\n", MAIN_INTERVAL_SECONDS);
 }
 
 /*----------------------------------------------------------------------------*/
-
 PROCESS_THREAD(ranger_process, ev, data)
 {
+    button_hal_button_t *btn;
+    static bool long_press_flag = false; // static storage class => retain value between yielding
+
     PROCESS_BEGIN();
+    
+    LOG_INFO("Waiting for TAISC initialisation\n");
+    etimer_set(&taisc_init_delay_tmr, CLOCK_SECOND*2);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&taisc_init_delay_tmr));
+    LOG_INFO("TAISC is initialised\n");
 
     if (ENABLE_SEND_PIN)
     {
         init_send_pin();
     }
-
+    
     set_tx_power(TX_POWER_DBM);
     set_channel(CHANNEL);
+    //set_mcs_index(MCS_INDEX_0);
 
     print_diagnostics();
     LOG_INFO("Started ranger process\n");
 
     leds_off(LEDS_ALL);
 
-    nullnet_set_input_callback(received_nullnet_message_callback);
+    ranger_net_set_input_callback(received_ranger_net_message_callback);
 
     current_mode = RX;
     LOG_INFO("Booted in RX Mode\n");
@@ -349,18 +354,44 @@ PROCESS_THREAD(ranger_process, ev, data)
     {
         PROCESS_YIELD();
 
-        if (ev == button_hal_release_event)
+        if (ev == button_hal_periodic_event)
         {
-            toggle_mode();
+            LOG_INFO("Periodic button event\n");
+            btn = (button_hal_button_t *)data;
+            if (btn->press_duration_seconds == 5)
+            {
+                LOG_INFO("Pressed user button for 5 seconds\n");
+                long_press_flag = true;
+                toggle_mode();
+            }
         }
-
-        if (etimer_expired(&message_send_tmr))
+        else if (ev == button_hal_release_event)
+        {
+            if (!long_press_flag)
+            {
+                btn = (button_hal_button_t *)data;
+                if (btn == button_hal_get_by_id(BUTTON_HAL_ID_USER_BUTTON))
+                {
+                    LOG_INFO("Released user button\n");
+                    //set_mcs_index((current_mcs_index + 1) % MCS_INDEX_AMOUNT);
+                }
+            }
+            else
+            {
+                long_press_flag = false;
+            }
+        }
+        else if (ENABLE_SEND_PIN && ev == send_pin_event) 
+        {
+            send_message(&linkaddr_null);
+        }
+        else if (etimer_expired(&message_send_tmr))
         {
             if (current_mode == TX)
             {
-                send_message();
+                send_message(&linkaddr_null);
             }
-
+            
             etimer_reset(&message_send_tmr);
         }
     }
