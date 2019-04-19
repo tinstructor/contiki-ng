42936c0db59ccac1481b63bb0b75e9f5da296f5c
relsas <robbe.elsas@ugent.be>
2019-04-19 12:49:20 +0200

    Added radio driver config options

--- examples/ranger/project-conf.h
+++ examples/ranger/project-conf.h
@@ -4,7 +4,7 @@
 #undef  IEEE802154_CONF_PANID
 #define IEEE802154_CONF_PANID   0x6689
 
-//#define LOG_CONF_LEVEL_MAC          LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_MAC          LOG_LEVEL_INFO
 //#define LOG_CONF_LEVEL_RANGER       LOG_LEVEL_INFO
 //#define LOG_CONF_LEVEL_RANGER_NET   LOG_LEVEL_INFO
 
@@ -14,15 +14,19 @@
 
 #undef NETSTACK_CONF_RADIO
 #ifndef RADIO_2400MHZ
+    /*
+    * You will need to configure the defines below to match the configuration of
+    * your sub-ghz network.
+    */
     #define NETSTACK_CONF_RADIO             cc1200_driver
     #define CC1200_CONF_RF_CFG              cc1200_802154g_863_870_fsk_50kbps
     //#define CC1200_CONF_RF_CFG              cc1200_868_fsk_1_2kbps
     #define ANTENNA_SW_SELECT_DEF_CONF      ANTENNA_SW_SELECT_SUBGHZ
     #define CC1200_CONF_USE_GPIO2           0
     #define CC1200_CONF_USE_RX_WATCHDOG     0
-
-    #define CSMA_CONF_ACK_WAIT_TIME                 (RTIMER_SECOND / 200)
-    #define CSMA_CONF_AFTER_ACK_DETECTED_WAIT_TIME  (RTIMER_SECOND / 1500)
+    #define CC1200_CONF_802154G             0
+    #define CC1200_CONF_802154G_CRC16       0
+    #define CC1200_CONF_802154G_WHITENING   0
 #else
     #define NETSTACK_CONF_RADIO              cc2538_rf_driver
     #define ANTENNA_SW_SELECT_DEF_CONF       ANTENNA_SW_SELECT_2_4GHZ
