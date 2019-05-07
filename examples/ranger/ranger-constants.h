d9adee30aa3405bc4079e210b860378e8d902bef
relsas <robbe.elsas@ugent.be>
2019-05-07 13:03:19 +0200

    Finalized new RF config

--- examples/ranger/ranger-constants.h
+++ examples/ranger/ranger-constants.h
@@ -33,12 +33,14 @@ extern const cc1200_rf_cfg_t cc1200_868_2fsk_1_2kbps;
 extern const cc1200_rf_cfg_t cc1200_868_2gfsk_50kbps;
 extern const cc1200_rf_cfg_t cc1200_868_2gfsk_100kbps;
 extern const cc1200_rf_cfg_t cc1200_868_4gfsk_200kbps;
+extern const cc1200_rf_cfg_t cc1200_868_2gfsk_500kbps;
 extern const cc1200_rf_cfg_t cc1200_868_4gfsk_1000kbps;
 
 static const cc1200_rf_cfg_t * const rf_cfg_ptrs[] = {&cc1200_868_2fsk_1_2kbps, 
                                                       &cc1200_868_2gfsk_50kbps,
                                                       &cc1200_868_2gfsk_100kbps,
                                                       &cc1200_868_4gfsk_200kbps,
+                                                      &cc1200_868_2gfsk_500kbps,
                                                       &cc1200_868_4gfsk_1000kbps};
 
 enum {RF_CFG_AMOUNT = sizeof(rf_cfg_ptrs)/sizeof(rf_cfg_ptrs[0]),};
