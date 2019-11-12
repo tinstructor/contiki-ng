a536f79b8621991a76acf2c7c1593c3c70d3426e
relsas <robbe.elsas@ugent.be>
2019-11-12 17:12:29 +0100

    Now using pexpect to control make login terminal

--- examples/ranger/button-press.py
+++ examples/ranger/button-press.py
@@ -3,6 +3,8 @@
 #   button-press.py   11-12-2019
 #    
 
+import pexpect
+
 import RPi.GPIO as gpio
 from subprocess import call
 import time
@@ -10,10 +12,13 @@ import time
 gpio.setmode(gpio.BCM)
 gpio.setup(26, gpio.IN, pull_up_down = gpio.PUD_UP)
 
-def set_backlight(channel):
-    print("Hello world!")
+def start_test(channel):
+    child = pexpect.spawn('make login')
+    child.expect('^.*?[OK].*?\r\n')
+    child.sendline('l')
+    child.interact()
     
-gpio.add_event_detect(26, gpio.FALLING, callback=set_backlight, bouncetime=300)
+gpio.add_event_detect(26, gpio.FALLING, callback=start_test, bouncetime=300)
 
 while 1:
     time.sleep(360)
