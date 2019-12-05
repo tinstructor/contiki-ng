695fb6d2176894fb51942a22795f39048c9c053a
relsas <robbe.elsas@ugent.be>
2019-12-05 15:58:54 +0100

    Second try at rebooting UWB

--- examples/ranger/button-press.py
+++ examples/ranger/button-press.py
@@ -34,6 +34,7 @@ def run_uwb():
     cmd = "/home/pi/Downloads/JLink_Linux_V620h_arm/JLinkRTTLogger -device CC2538SF53 -if JTAG -speed 50000 -RTTChannel 0 %s.log" % (datetime.datetime.now().strftime("log_uwb_%d-%m-%Y_%H-%M-%S-%f"))
     global shell_2
     shell_2 = subprocess.Popen(shlex.split(cmd),stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,universal_newlines=True,bufsize=0)
+    shell_2.terminate()
     uwb_timer.start()
 
 def start_test(channel):
