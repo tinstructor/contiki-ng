a86a409e37c8cd0496687b99aad9ac76ab708080
relsas <robbe.elsas@ugent.be>
2019-12-05 16:53:15 +0100

    Rebooting UWB every hour

--- examples/ranger/button-press.py
+++ examples/ranger/button-press.py
@@ -25,7 +25,7 @@ gpio.setup(16, gpio.IN, pull_up_down = gpio.PUD_UP)
 gpio.setup(25, gpio.OUT, initial = gpio.HIGH)
 
 test_running = False
-uwb_timer = threading.Timer(10, run_uwb)
+uwb_timer = None
 
 def run_uwb():
     gpio.output(25, 0)
@@ -34,7 +34,8 @@ def run_uwb():
     cmd = "/home/pi/Downloads/JLink_Linux_V620h_arm/JLinkRTTLogger -device CC2538SF53 -if JTAG -speed 50000 -RTTChannel 0 %s.log" % (datetime.datetime.now().strftime("log_uwb_%d-%m-%Y_%H-%M-%S-%f"))
     global shell_2
     shell_2 = subprocess.Popen(shlex.split(cmd),stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,universal_newlines=True,bufsize=0)
-    shell_2.terminate()
+    global uwb_timer
+    uwb_timer = threading.Timer(3600, run_uwb)
     uwb_timer.start()
 
 def start_test(channel):
@@ -48,7 +49,8 @@ def start_test(channel):
 def reset_node(channel):
     shell_0.stdin.write("r")
     shell_1.stdin.write("r")
-    uwb_timer.cancel()
+    if isinstance(uwb_timer, threading.Timer):
+        uwb_timer.cancel()
     global test_running
     test_running = False
     if isinstance(shell_2, subprocess.Popen):
