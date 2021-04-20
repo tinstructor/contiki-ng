052ac3caf621eb90913f41cf4087e00b5ff042e9
relsas <robbe.elsas@ugent.be>
2021-04-20 18:19:20 +0200

    Improved starting point for DRiPL OF implementation

--- os/net/routing/rpl-classic/rpl-driplof.c
+++ os/net/routing/rpl-classic/rpl-driplof.c
@@ -46,6 +46,11 @@
 #define LOG_MODULE "RPL"
 #define LOG_LEVEL LOG_LEVEL_RPL
 
+#define DRIPL_ETX_DIVISOR         LINK_STATS_ETX_DIVISOR
+#define MAX_LINK_METRIC           (8U * DRIPL_ETX_DIVISOR)
+#define PARENT_SWITCH_THRESHOLD   (.75F * DRIPL_ETX_DIVISOR)
+#define MAX_PATH_COST             (256U * DRIPL_ETX_DIVISOR)
+
 /*---------------------------------------------------------------------------*/
 static void
 reset(rpl_dag_t *dag)
@@ -64,43 +69,105 @@ dao_ack_callback(rpl_parent_t *p, int status)
 static uint16_t
 parent_link_metric(rpl_parent_t *p)
 {
-  return 0xffff;
-}
-/*---------------------------------------------------------------------------*/
-static uint16_t
-parent_rank_increase(rpl_parent_t *p)
-{
-  return RPL_INFINITE_RANK;
+  if(p == NULL) {
+    return 0xffff;
+  }
+
+  const struct link_stats *stats = rpl_get_parent_link_stats(p);
+  return stats != NULL ? stats->normalized_metric : 0xffff;
 }
 /*---------------------------------------------------------------------------*/
 static uint16_t
 parent_path_cost(rpl_parent_t *p)
 {
-  return 0xffff;
+  uint16_t base;
+
+  if(p == NULL || p->dag == NULL || p->dag->instance == NULL) {
+    return 0xffff;
+  }
+
+#if RPL_WITH_MC
+  /* Handle the different MC types */
+  switch(p->dag->instance->mc.type) {
+    /* TODO add case for yet to be defined metric container */
+    default:
+      base = p->rank;
+      break;
+  }
+#else /* RPL_WITH_MC */
+  base = p->rank;
+#endif /* RPL_WITH_MC */
+
+  /* path cost upper bound: 0xffff */
+  return MIN((uint32_t)base + parent_link_metric(p), 0xffff);
 }
 /*---------------------------------------------------------------------------*/
 static rpl_rank_t
 rank_via_parent(rpl_parent_t *p)
 {
-  return RPL_INFINITE_RANK;
+  uint16_t min_hoprankinc;
+  uint16_t path_cost;
+
+  if(p == NULL || p->dag == NULL || p->dag->instance == NULL) {
+    return RPL_INFINITE_RANK;
+  }
+
+  min_hoprankinc = p->dag->instance->min_hoprankinc;
+  path_cost = parent_path_cost(p);
+
+  /* Rank lower-bound: parent rank + min_hoprankinc */
+  return MAX(MIN((uint32_t)p->rank + min_hoprankinc, 0xffff), path_cost);
 }
 /*---------------------------------------------------------------------------*/
 static int
 parent_is_acceptable(rpl_parent_t *p)
 {
-  return false;
+  uint16_t link_metric = parent_link_metric(p);
+  uint16_t path_cost = parent_path_cost(p);
+  /* Exclude links with too high link metrics or path cost (RFC6719, 3.2.2) */
+  return link_metric <= (MAX_LINK_METRIC) && path_cost <= MAX_PATH_COST;
 }
 /*---------------------------------------------------------------------------*/
 static int
 parent_has_usable_link(rpl_parent_t *p)
 {
-  return false;
+  uint16_t link_metric = parent_link_metric(p);
+  /* Exclude links with too high link metrics  */
+  return link_metric <= MAX_LINK_METRIC;
 }
 /*---------------------------------------------------------------------------*/
 static rpl_parent_t *
 best_parent(rpl_parent_t *p1, rpl_parent_t *p2)
 {
-  return NULL;
+  rpl_dag_t *dag;
+  uint16_t p1_cost;
+  uint16_t p2_cost;
+  int p1_is_acceptable;
+  int p2_is_acceptable;
+
+  p1_is_acceptable = p1 != NULL && parent_is_acceptable(p1);
+  p2_is_acceptable = p2 != NULL && parent_is_acceptable(p2);
+
+  if(!p1_is_acceptable) {
+    return p2_is_acceptable ? p2 : NULL;
+  }
+  if(!p2_is_acceptable) {
+    return p1_is_acceptable ? p1 : NULL;
+  }
+
+  dag = p1->dag; /* Both parents are in the same DAG. */
+  p1_cost = parent_path_cost(p1);
+  p2_cost = parent_path_cost(p2);
+
+  /* Maintain stability of the preferred parent in case of similar ranks. */
+  if(p1 == dag->preferred_parent || p2 == dag->preferred_parent) {
+    if(p1_cost < p2_cost + PARENT_SWITCH_THRESHOLD &&
+       p1_cost > p2_cost - PARENT_SWITCH_THRESHOLD) {
+      return dag->preferred_parent;
+    }
+  }
+
+  return p1_cost < p2_cost ? p1 : p2;
 }
 /*---------------------------------------------------------------------------*/
 static rpl_dag_t *
@@ -117,11 +184,48 @@ best_dag(rpl_dag_t *d1, rpl_dag_t *d2)
   return d1->rank < d2->rank ? d1 : d2;
 }
 /*---------------------------------------------------------------------------*/
+#if !RPL_WITH_MC
 static void
 update_metric_container(rpl_instance_t *instance)
 {
   instance->mc.type = RPL_DAG_MC_NONE;
 }
+#else /* RPL_WITH_MC */
+static void
+update_metric_container(rpl_instance_t *instance)
+{
+  rpl_dag_t *dag;
+  uint16_t path_cost;
+  uint8_t type;
+
+  dag = instance->current_dag;
+  if(dag == NULL || !dag->joined) {
+    LOG_WARN("Cannot update the metric container when not joined\n");
+    return;
+  }
+
+  if(dag->rank == ROOT_RANK(instance)) {
+    /* Configure MC at root only, other nodes are auto-configured when joining */
+    instance->mc.type = RPL_DAG_MC;
+    instance->mc.flags = 0;
+    instance->mc.aggr = RPL_DAG_MC_AGGR_ADDITIVE;
+    instance->mc.prec = 0;
+    path_cost = dag->rank;
+  } else {
+    path_cost = parent_path_cost(dag->preferred_parent);
+  }
+
+  /* Handle the different MC types */
+  switch(instance->mc.type) {
+    case RPL_DAG_MC_NONE:
+      break;
+    /* TODO add case for yet to be defined metric container */
+    default:
+      LOG_WARN("MRHOF, non-supported MC %u\n", instance->mc.type);
+      break;
+  }
+}
+#endif /* RPL_WITH_MC */
 /*---------------------------------------------------------------------------*/
 rpl_of_t rpl_driplof = {
   reset,
